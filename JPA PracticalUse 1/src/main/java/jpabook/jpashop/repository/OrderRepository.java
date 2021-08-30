3e85460b4ed2ec527ee994a329d8992a6966591e
6161990 <6161990src@gmail.com>
2021-08-30 20:51:46 +0900

    주문 검색 기능 개발 동적 쿼리 이용

--- JPA PracticalUse 1/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ JPA PracticalUse 1/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,10 +1,16 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -20,5 +26,84 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    //public List<Order> findAll(OrderSearch orderSearch){ ... }
+    public List<Order> findAllByString(OrderSearch orderSearch){
+        // Order 테이블과 관련있는 member를 조인해라, 하지만 이렇게 했을 때 orderStatus와 memberName이 없다면?
+        // 없는 파라미터를 필터링 where 조건으로 쓰지 말고 그냥 다 가져와 라고 동적 으로 움직일 수 있어야함.
+        // 그러나 아래 쿼리는 정적으로 된 쿼리
+/*        return em.createQuery("select o from Order o join o.member m" +
+                " where o.status = :status " +
+                " and m.name like :name ", Order.class)
+                .setParameter("status",orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                //.setFirstResult() 가져오는 시작점
+                .setMaxResults(1000) //가져오는 마지막 지점 : 최대 1000건
+                .getResultList();*/
+
+        //이제 정적쿼리 근데 이제 복잡함을 곁들인..
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+                .setMaxResults(1000);
+
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+
+        return query.getResultList();
+    }
+
+    /**
+     * JPA Criteria
+     */
+    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Object, Object> m = o.join("member", JoinType.INNER);
+
+        List<Predicate> criteria = new ArrayList<>();
+
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            Predicate name =
+                    cb.like(m.<String>get("name"), "%" + orderSearch.getMemberName() + "%");
+            criteria.add(name);
+        }
+
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
+        return query.getResultList();
+    }
+
 }
