300a7644a3fc14b4d893f3db52d32d6caf7524bb
6161990 <6161990src@gmail.com>
2021-09-08 13:24:12 +0900

    간단한 jpa , jpql 원리

--- JPA-Basic/src/main/java/jpabasic/ex1hellojpa/JpaMain.java
+++ JPA-Basic/src/main/java/jpabasic/ex1hellojpa/JpaMain.java
@@ -2,14 +2,35 @@ package jpabasic.ex1hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
 
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager entityManager = emf.createEntityManager();
-        entityManager.close();
-        emf.close();
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+
+        try{
+/*            Member member = new Member();
+            member.setId(2L);
+            member.setName("리사");
+
+            entityManager.persist(member);*/
+
+            Member member1 = entityManager.find(Member.class, 1L);
+            member1.setName("로제");
+
+            transaction.commit();
+        } catch (Exception e){
+            transaction.rollback();
+        }finally {
+            entityManager.close();
+        }
+        entityManagerFactory.close();
     }
 }
