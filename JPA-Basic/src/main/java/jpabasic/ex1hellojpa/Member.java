a5beaa994447962c0cc4ccef9fae57300dffa661
6161990 <6161990src@gmail.com>
2021-09-08 16:12:27 +0900

    엔티티 매핑 , DB 스키마 자동생성, 필드와 컬럼 매핑

--- JPA-Basic/src/main/java/jpabasic/ex1hellojpa/Member.java
+++ JPA-Basic/src/main/java/jpabasic/ex1hellojpa/Member.java
@@ -1,19 +1,47 @@
 package jpabasic.ex1hellojpa;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
 
     @Id
     private Long id;
-    private String name;
+
+    @Column(name = "name")
+    private String username;
+
+    private int age;
+
+    @Column(precision = 30)
+    private BigDecimal testAge;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Transient
+    private int temp;
+
+    @Lob
+    private String description;
+
 
 
 }
