ef138bf0605845e71e97ef0c7eedbb5b31f5f1c0
6161990 <6161990src@gmail.com>
2021-09-23 11:03:47 +0900

    값 타입 비교 (동등성 비교 사용하기)

--- JPA-Basic/src/main/java/jpabasic/ex1hellojpa/Address.java
+++ JPA-Basic/src/main/java/jpabasic/ex1hellojpa/Address.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
 @Getter //@Setter 불변 객체로 설계
@@ -17,4 +18,18 @@ public class Address {
     private String street;
     private String zipcode;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(city, address.city) &&
+                Objects.equals(street, address.street) &&
+                Objects.equals(zipcode, address.zipcode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, zipcode);
+    }
 }
