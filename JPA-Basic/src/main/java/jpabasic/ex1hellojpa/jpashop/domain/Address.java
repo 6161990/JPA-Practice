659b614a4ed357c16f7df9a715f2777700beb28a
6161990 <6161990src@gmail.com>
2021-09-24 17:28:49 +0900

    값 타입 실전 적용 plus.Address valid

--- JPA-Basic/src/main/java/jpabasic/ex1hellojpa/jpashop/domain/Address.java
+++ JPA-Basic/src/main/java/jpabasic/ex1hellojpa/jpashop/domain/Address.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import java.util.Objects;
 
@@ -13,9 +14,18 @@ import java.util.Objects;
 @AllArgsConstructor
 public class Address {
 
+    @Column(length = 10)
     private String city;
+
+    @Column(length = 20)
     private String street;
+
+    @Column(length = 5)
     private String zipcode;
+
+    public String fullAddress() {
+        return getCity() + " " + getStreet() + " " + getZipcode();
+    }
 
     @Override
     public boolean equals(Object o) {
