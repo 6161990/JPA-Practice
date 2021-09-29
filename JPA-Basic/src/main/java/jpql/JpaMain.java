20095da889fa9ce0bfeecab5166e40222622b416
6161990 <6161990src@gmail.com>
2021-09-29 09:07:22 +0900

    JPQL 함수, 사용자 정의 함수 호출

--- JPA-Basic/src/main/java/jpql/JpaMain.java
+++ JPA-Basic/src/main/java/jpql/JpaMain.java
@@ -21,23 +21,21 @@ public class JpaMain {
             entityManager.persist(team);
 
             jpql_Member jqqlMember = new jpql_Member();
-            jqqlMember.setUsername("관리자");
+            jqqlMember.setUsername("관리자1");
             jqqlMember.setAge(12);
             jqqlMember.setTeam(team);
             jqqlMember.setType(MemberType.ADMIN);
             entityManager.persist(jqqlMember);
 
+            jpql_Member jqqlMember2 = new jpql_Member();
+            jqqlMember2.setUsername("관리자2");
+            entityManager.persist(jqqlMember2);
+
             entityManager.flush();
             entityManager.clear();
 
-            String query1 = "select case when m.age <= 10 then '학생요금'" +
-                                        "when m.age >= 60 then '경로요금' " +
-                                        "else '일반요금' end " +
-                           "from jpql_Member m";
 
-            String query2 = "select coalesce(m.username, '이름 없는 회원') from jpql_Member m";
-
-            String query ="select nullif(m.username, '관리자') as username from jpql_Member m";
+            String query ="select function('group_concat', m.username) from jpql_Member m";
             List<String> resultList = entityManager.createQuery(query, String.class).getResultList();
 
             for(String s : resultList){
